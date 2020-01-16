65a8e9760c3bd0182d3d6fd3a0cda08695a36e69
anthonysea <anthonyqchao@gmail.com>
2020-01-15 16:14:03 -0800

    Added index page with record counts of some queries using function-based views; added list view and detail view of books using django.views generics (class-based views)

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance, Genre
+from django.db.models import Q
+from django.views import generic
 
-# Create your views here.
+def index(request):
+    """View function for home page of the site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default
+    num_authors = Author.objects.count()
+
+    num_fictional_books = Book.objects.filter(~Q(genre__name__contains='Non-fiction')).count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_fictional_books': num_fictional_books
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    ## Can define context_object, queryset, and template_name
+    # context_object_name = 'my_book_list' # your own name for the list as a template variable
+    # queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    # template_name = 'books/my_arbitrary_template_name_list.html' # Specify your own template name/location
+    template_name = 'book_list.html'
+
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get the context
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Create any data and add it to the context
+    #     context['some_data'] = 'This is just some data'
+    #     return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book_detail.html'
