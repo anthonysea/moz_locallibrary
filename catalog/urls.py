5d01ad8140d954773d04a30a6388b95682687208
anthonysea <anthonyqchao@gmail.com>
2020-01-20 14:01:59 -0800

    Starting creating forms for book renewal; started with Form and View

--- catalog/urls.py
+++ catalog/urls.py
@@ -18,3 +18,8 @@ urlpatterns += [
 urlpatterns += [
     path('borrowedbooks/', views.LoanedBooksView.as_view(), name='borrowed')
 ]
+
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
